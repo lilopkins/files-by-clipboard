132bb3dcbb505f775dc708576eac2f7f158ba251
Lily Hopkins <lily@hpkns.uk>
2023-05-17 14:14:51 +0100

    feat: added encode/decode function

--- src/Decode.tsx
+++ src/Decode.tsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 
+function base64ToArrayBuffer(base64: string): ArrayBuffer {
+  const binaryString = atob(base64)
+  const bytes = new Uint8Array(binaryString.length)
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i)
+  }
+  return bytes.buffer
+}
+
 function Decode() {
+  const [input, setInput] = useState<string>('')
+
+  const download = () => {
+    const sections = input.split(':')
+    if (sections.length != 3) {
+      alert('Invalid encoded data! Please try again.')
+      return
+    }
+
+    const fileName = sections[0]
+    const data = sections[1]
+    const checksum = sections[2]
+
+    if (checksum !== 'end') {
+      alert('Corrupted encoded data! Please try again.')
+      return
+    }
+
+    const file = new File([base64ToArrayBuffer(data) as BlobPart], fileName)
+
+    const a = document.createElement('a')
+    a.href = URL.createObjectURL(file)
+    a.download = fileName
+    a.click()
+  }
+
   return (
     <Col>
       <h2>Decode</h2>
       <Form>
         <FloatingLabel label='Encoded File Data'>
-          <Form.Control as='textarea' style={{ height: '200px' }} />
+          <Form.Control as='textarea' style={{ height: '200px' }} onChange={(e) => setInput(e.currentTarget.value)} />
         </FloatingLabel>
-        <Button className='mt-2' type='submit' variant='primary'>Download</Button>
+        <Button className='mt-2' type='submit' variant='primary' onClick={download}>Download</Button>
       </Form>
     </Col>
   )
--- src/Encode.tsx
+++ src/Encode.tsx
@@ -1,19 +1,60 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Button from 'react-bootstrap/Button'
 
+const NO_DATA = 'No file selected'
+const PROCESSING = 'Processing file...'
+
+function arrayBufferToBase64(buffer: ArrayBuffer) {
+  let binary = ''
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return window.btoa(binary)
+}
+
 function Encode() {
+  const [encodedData, setEncodedData] = useState<string>(NO_DATA)
+  const [flash, _setFlash] = useState<string>()
+
+  const encodeFile = async (e: ChangeEvent<HTMLInputElement>) => {
+    setEncodedData(PROCESSING)
+    const files = e.currentTarget.files
+    if (files === null) {
+      setEncodedData(NO_DATA)
+      return
+    }
+
+    const file = files[0]
+    const buffer = await file.arrayBuffer()
+    const b64encoded = arrayBufferToBase64(buffer)
+    setEncodedData(`${file.name.replaceAll(':', '_')}:${b64encoded}:end`)
+  }
+
+  const setFlash = (flash: string) => {
+    _setFlash(flash)
+    setTimeout(() => _setFlash(undefined), 500)
+  }
+
+  const copy = async (val: string) => {
+    await navigator.clipboard.writeText(val)
+    setFlash('Copied!')
+  }
+
   return (
     <Col>
       <h2>Encode</h2>
-      <Form.Control type='file' />
+      <Form.Control type='file' onChange={(e) => encodeFile(e as ChangeEvent<HTMLInputElement>)} />
       <FloatingLabel className='mt-2' label='Encoded File Data'>
-        <Form.Control as='textarea' readOnly={true} style={{ height: '200px' }} />
+        <Form.Control as='textarea' readOnly={true} style={{ height: '200px' }} value={encodedData}></Form.Control>
       </FloatingLabel>
-      <Button className='mt-2' variant='primary'>Copy</Button>
-      <p>Copied!</p>
+      <Button className='mt-2' variant='primary' onClick={() => copy(encodedData)}>Copy</Button>
+      <p>{flash}&nbsp;</p>
     </Col>
   )
 }
